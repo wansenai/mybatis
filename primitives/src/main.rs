9a0e60040dded74273c098a05348eb5ca668b45a
JamesZow <Jameszow@163.com>
2021-03-22 19:26:22 +0800

    add new TextAndOperators.rs chapter 2

---
+++ primitives/src/main.rs
@@ -0,0 +1,17 @@
+mod TextAndOperators;
+
+fn main() {
+    let logical : bool = true;
+
+    let price : f64 = 185.36;
+    let number = 5i32;
+    let default_float = 3.6;
+    let default_integer = 723123;
+
+    let mut numberA = 5;
+    numberA = 15;
+
+    let numberA = true;
+
+    println!("{:?} {:?}", price , numberA)
+}
