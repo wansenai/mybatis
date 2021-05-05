a70a4232b2c73013dcf62ace06015d532c707fab
JamesZow <Jameszow@163.com>
2021-05-05 14:21:05 +0800

    new add Closures.rs

--- Function/src/Closures.rs
+++ Function/src/Closures.rs
@@ -23,3 +23,59 @@ fn main(){
     let var =  || 1;
     println!("普通var : {}", var());
 }
+
+
+///
+/// 捕捉 闭包本质上是灵活的，闭包也可以捕获某个变量
+/// 引用变量: &T    可修改变量: &mut T    按值: T
+///
+
+#[test]
+fn capturing_test(){
+    use std::mem;
+
+    let color = String::from("red");
+    // 打印color 的 闭包，color将引用&color，和闭包的print
+    let print = || println!("颜色: {}", color);
+    print();
+
+    // 对color不可变的引用
+    let var_one = &color;
+    print();
+
+    // 把color绑定刀新的变量上
+    let var_two = color;
+    let mut count = 0;
+
+    let mut inc = ||{
+        count += 1;
+        println!("count : {}", count)
+    };
+    // 调用inc闭包函数
+    inc();
+
+    let movable = Box::new(3);
+    let consume = || {
+        println!("Box :{:?}", movable);
+        mem::drop(movable);
+    };
+
+    consume();
+}
+
+///
+/// move 可以强制关闭从闭包中已获取和已捕获的变量所有权
+///
+
+#[test]
+fn move_test(){
+    let var_list = vec![1, 7, 8];
+
+    let contains = move |needle| var_list.contains(needle);
+
+    println!("{}", contains(&1));
+    println!("{}", contains(&4));
+
+    // 打印数组长度会报错，只有移除move，将导致闭包，就可以引用var_list变量
+    // println!("数组长度: {:?}", var_list.len());
+}
