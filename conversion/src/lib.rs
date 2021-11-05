aebaed8461fe5a0346928412c54dd695b306d79a
zhaowei <wei.zhao@iston.ai>
2021-11-05 13:24:57 +0800

    update lowercase name

--- conversion/src/lib.rs
+++ conversion/src/lib.rs
@@ -3,7 +3,7 @@ mod TryFromAndTryInto;
 mod ToFromStrings;
 
 ///
-/// Conversion 转换，原始类型可以通过强制转换改变类型
+/// conversion 转换，原始类型可以通过强制转换改变类型
 /// Rust 通过使用trait 解决了自定义类型（struct和enum）之间的转换
 /// 然而对于更常见的情况，有具体的办法，尤其是在转换String字符串的时候
 ///
