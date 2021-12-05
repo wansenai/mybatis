2f9efd2a309651b66e9089c2d42163026d644c76
jameszow <jameszow@163.com>
2021-12-05 15:32:12 +0800

    add cfg test utils test

--- microsoft/src/UtilTest.rs
+++ microsoft/src/UtilTest.rs
@@ -7,3 +7,22 @@ fn main(){
     assert_eq!(add(5,6), 11);
     assert_eq!(add(7,9), 16);
 }
+
+pub fn is_eve(num : i32) -> bool{
+    num % 2 == 0
+}
+
+#[cfg(test)]
+mod test{
+    use super::*;
+
+    #[test]
+    fn is_true_fun() {
+        assert!(is_eve(6));
+    }
+
+    // error
+    fn is_false_fun(){
+        assert!(is_eve(5));
+    }
+}
