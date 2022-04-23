2fb369bea54e2594dd128c11acf6d27da20ef128
jameszow <jameszow@163.com>
2022-04-24 01:19:14 +0800

    add #[sarde(default)] If the value is not present when deserializing, use the Default::default()

--- web/covid_19/src/domain/user.rs
+++ web/covid_19/src/domain/user.rs
@@ -2,12 +2,26 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
 pub struct User {
+    // id 主键
+    #[serde(default)]
     pub id: String,
+    // 用户名
+    #[serde(default)]
     pub username: String,
+    // 密码
+    #[serde(default)]
     pub password: String,
+    // 姓名
+    #[serde(default)]
     pub name: String,
+    // 性别（0-女，1-男 默认0）
+    #[serde(default)]
     pub sex: u8,
+    // 生日
+    #[serde(default)]
     pub brithday: String,
+    // 状态（0-启用，1-停用 默认0）
+    #[serde(default)]
     pub status: u8,
 }
 
