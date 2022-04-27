77979edb8ebf7889187e047e06f7e6c23f5b5b93
jameszow <jameszow@163.com>
2022-04-27 20:27:48 +0800

    add new code Service

--- web/covid_19/src/service/mod.rs
+++ web/covid_19/src/service/mod.rs
@@ -29,4 +29,29 @@ pub trait NucleicService {
      * 核酸登记
      */
     fn nucleic_registe(&self) -> bool;
+
+}
+
+// 核酸机构服务
+pub trait NucleicInstitutionService {
+
+    /**
+     * 注册核酸机构
+     */
+    fn insert_nucleic_institution(&self) -> bool;
+
+    /**
+     * 修改核酸机构
+     */
+    fn update_nucleic_institution(&self) -> bool;
+
+    /**
+     * 根据地区查询核酸机构
+     */
+    fn query_nucleic_institution_byregion(region: &str) -> bool;
+
+    /**
+     * 根据机构名称查询核酸机构
+     */
+    fn query_nucleic_institution_byname(name: &str) -> bool;
 }
