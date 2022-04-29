cc1b17aa06ee79063ee4127f007a4fc17c2ddd72
jameszow <jameszow@163.com>
2022-04-30 00:26:24 +0800

    fix new mysql version connection code

--- web/covid_19/src/common/dbconfig.rs
+++ web/covid_19/src/common/dbconfig.rs
@@ -3,10 +3,12 @@ use mysql::*;
 const DB_URL: &'static str = "mysql://root:passw0rd@localhost:3306/COVID-19";
 
 pub fn get_conn() -> Result<PooledConn> {
-    let pool = Pool::new(DB_URL)?;
-    let conn = pool.get_conn()?;
 
-    Ok(conn)
+    let opts = Opts::from_url(DB_URL).unwrap();
+    let pool = Pool::new(opts).unwrap();
+    let conn1 = pool.try_get_conn(357).unwrap();
+
+    Ok(conn1)
 }
 
 #[cfg(test)]
