8ce05a276ad96760623beef6f457e4d98ecdc5d9
James Zow <jameszow@163.com>
2022-05-13 19:37:31 +0800

    add macro test

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -1,7 +1,7 @@
 #[macro_use]
-extern crate mybatis_macro;
+extern crate mybatis;
 
-use mybatis::mybatis::Mybatis;
+mod pets;
 
 #[cfg(test)]
 mod test {
@@ -9,26 +9,4 @@ mod test {
     fn test() {
         assert_eq!(4+4, 8);
     }
-}
-
-/// this is table model(see ../database.sql)
-
-#[derive(CRUDTable, Clone, Debug)]
-pub struct BizActivity {
-    pub id: Option<String>,
-    pub name: Option<String>,
-    pub pc_link: Option<String>,
-    pub h5_link: Option<String>,
-    pub pc_banner_img: Option<String>,
-    pub h5_banner_img: Option<String>,
-    pub sort: Option<String>,
-    pub status: Option<i32>,
-    pub remark: Option<String>,
-    pub version: Option<i64>,
-    pub delete_flag: Option<i32>,
-}
-
-#[py_sql("example/example.html")]
-pub async fn py_select_rb(mybatis: &Mybatis, name: &str) -> Option<BizActivity> {
-    
 }
